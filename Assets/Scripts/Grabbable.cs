a0b839e50d5a52400d16c42f99286326d249a63f
MisterIdle <alexyhoublo@gmail.com>
2025-02-11 23:05:22 +0100

    Better grab, couch, random pos spawn (last commit)

--- Assets/Scripts/Grabbable.cs
+++ Assets/Scripts/Grabbable.cs
@@ -6,23 +6,29 @@ public class Grabbable : MonoBehaviour
     private Rigidbody _rigidbody;
     private Transform _grabPoint;
     private PlayerController _playerController;
+    private MeshCollider _meshCollider;
 
     [SerializeField] private float _lerpSpeed = 10f;
     [SerializeField] private float _releaseForceMultiplier = 2f;
 
     private Vector3 _velocity = Vector3.zero;
 
-    private void Start()
+    private void Awake()
     {
+        _meshCollider = GetComponent<MeshCollider>();
         _rigidbody = GetComponent<Rigidbody>();
         _playerController = FindFirstObjectByType<PlayerController>();
+
+        _meshCollider.convex = true;
+
+        _rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+        _rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
     }
 
     private void FixedUpdate()
     {
         if (_grabPoint != null)
         {
-            // Mouvement fluide vers le point de grab
             Vector3 targetPosition = _grabPoint.position;
             _rigidbody.MovePosition(Vector3.SmoothDamp(_rigidbody.position, targetPosition, ref _velocity, 1f / _lerpSpeed));
         }
